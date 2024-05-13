5fc38f6421092c27cca6bd0f465ec230d01dfbe8
Chikurin1014 <chikurin1014@gmail.com>
2024-05-13 23:52:23 +0900

    rename variables

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@ constexpr uint8_t PIN_ID_MOTOR_2_C = 21;
 constexpr uint8_t PIN_ID_MOTOR_PWM_C = 5;
 constexpr uint8_t CHANNEL_ID_MOTOR_C = 2;
 
-auto inputModules = Machine::InputModules{};
-auto outputModules =
+auto input = Machine::InputModules{};
+auto output =
   Machine::OutputModules{MotorDriver{PIN_ID_MOTOR_1_A, PIN_ID_MOTOR_2_A, PIN_ID_MOTOR_PWM_A, CHANNEL_ID_MOTOR_A},
                          MotorDriver{PIN_ID_MOTOR_1_B, PIN_ID_MOTOR_2_B, PIN_ID_MOTOR_PWM_B, CHANNEL_ID_MOTOR_B},
                          MotorDriver{PIN_ID_MOTOR_1_C, PIN_ID_MOTOR_2_C, PIN_ID_MOTOR_PWM_C, CHANNEL_ID_MOTOR_C}};
@@ -27,12 +27,12 @@ auto model = Machine::Logic::Model{};
 
 void setup() {
     model.init();
-    inputModules.begin();
-    outputModules.setup();
+    input.begin();
+    output.setup();
 }
 
 void loop() {
-    auto input = inputModules.read();
-    auto output = model.update(input);
-    outputModules.write(output);
+    auto i = input.read();
+    auto o = model.update(i);
+    output.write(o);
 }
