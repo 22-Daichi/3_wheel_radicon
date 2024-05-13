473078dfd98969077eaf9c7d39cb4b599569ac19
Chikurin1014 <chikurin1014@gmail.com>
2024-05-13 22:47:50 +0900

    add system model class

---
+++ include/OutputFrame.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Machine {
+
+struct OutputFrame {
+    int motorA;
+    int motorB;
+    int motorC;
+};
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <Ps3Controller.h>
 
 #include "InputModules.hpp"
+#include "Model.hpp"
 #include "Motordrive.hpp"
 
 using Machine::InputFrame;
 
 Machine::InputModules inputModules;
+Machine::Logic::Model model;
 
 constexpr int motorAp = 14;
 constexpr int motorBp = 25;
@@ -26,24 +28,8 @@ Motordrive motorA{motorA1, motorA2, motorAp, pwmch_A};
 Motordrive motorB{motorB1, motorB2, motorBp, pwmch_B};
 Motordrive motorC{motorC1, motorC2, motorCp, pwmch_C};
 
-int controller_x(InputFrame input) {
-    return input.controller.analog.stick.lx * -1.9;
-}
-int controller_y(InputFrame input) {
-    return input.controller.analog.stick.ly * -1.9;
-}
-
-int velocityA(InputFrame input) {
-    return controller_x(input);
-}
-int velocityB(InputFrame input) {
-    return (-1 * controller_x(input) / 2) + (controller_y(input) * 1.732 / 2);
-}
-int velocityC(InputFrame input) {
-    return (-1 * controller_x(input) / 2) + (-1 * controller_y(input) * 1.732 / 2);
-}
-
 void setup() {
+    model.init();
     inputModules.begin();
     motorA.setup();
     motorB.setup();
@@ -55,24 +41,8 @@ void setup() {
 
 void loop() {
     auto input = inputModules.read();
-    int v_a = velocityA(input);
-    int v_b = velocityB(input);
-    int v_c = velocityC(input);
-
-    if (input.controller.button.circle) {
-        v_a += 200;
-        v_b += 200;
-        v_c += 200;
-    } else if (input.controller.button.square) {
-        v_a -= 200;
-        v_b -= 200;
-        v_c -= 200;
-    } else if (input.controller.button.cross) {
-        v_a = 0;
-        v_b = 0;
-        v_c = 0;
-    }
-    motorA.DRIVE(v_a);
-    motorB.DRIVE(v_b);
-    motorC.DRIVE(v_c);
+    auto output = model.update(input);
+    motorA.DRIVE(output.motorA);
+    motorB.DRIVE(output.motorB);
+    motorC.DRIVE(output.motorC);
 }
