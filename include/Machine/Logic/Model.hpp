0aec11fe8108931e77641a29ba4617b0d08afd25
Chikurin1014 <chikurin1014@gmail.com>
2024-05-14 23:46:43 +0900

    remove magic number

--- include/Machine/Logic/Model.hpp
+++ include/Machine/Logic/Model.hpp
@@ -12,6 +12,10 @@ private:
     float machineAngle;
 
 public:
+    inline static constexpr float ANGLE_OFFSET_A = M_PI;
+    inline static constexpr float ANGLE_OFFSET_B = -M_PI / 3;
+    inline static constexpr float ANGLE_OFFSET_C = M_PI / 3;
+
     Model() = default;
 
     auto init() -> void;
--- src/Machine/Logic/Model.cpp
+++ src/Machine/Logic/Model.cpp
@@ -3,20 +3,20 @@
 auto Machine::Logic::Model::init() -> void {
     machineAngle = 0;
 }
-auto Machine::Logic::Model::update(Machine::InputFrame in) -> Machine::OutputFrame {
-    const auto &c = in.controller;
+auto Machine::Logic::Model::update(Machine::InputFrame input) -> Machine::OutputFrame {
+    const auto &c = input.controller;
 
     if (c.button.cross) {
-        return {0, 0, 0};
+        return Machine::OutputFrame{0, 0, 0};
     }
 
-    int x = c.analog.stick.lx * -1.9;
-    int y = c.analog.stick.ly * -1.9;
-    int omega = c.button.circle ? 200 : c.button.square ? -200 : 0;
+    const int x = c.analog.stick.lx * 1.9;
+    const int y = c.analog.stick.ly * 1.9;
+    const int omega = c.button.circle ? 200 : c.button.square ? -200 : 0;
 
-    Machine::OutputFrame out;
-    out.motorA = x * cos(-machineAngle) * y * sin(-machineAngle) + omega;
-    out.motorB = x * cos(-machineAngle + 2 * M_PI / 3) + y * sin(-machineAngle + 2 * M_PI / 3) + omega;
-    out.motorC = x * cos(-machineAngle - 2 * M_PI / 3) + y * sin(-machineAngle - 2 * M_PI / 3) + omega;
-    return out;
+    Machine::OutputFrame output;
+    output.motorA = x * cos(-machineAngle + ANGLE_OFFSET_A) * y * sin(-machineAngle + ANGLE_OFFSET_A) + omega;
+    output.motorB = x * cos(-machineAngle + ANGLE_OFFSET_B) + y * sin(-machineAngle + ANGLE_OFFSET_B) + omega;
+    output.motorC = x * cos(-machineAngle + ANGLE_OFFSET_C) + y * sin(-machineAngle + ANGLE_OFFSET_C) + omega;
+    return output;
 }
