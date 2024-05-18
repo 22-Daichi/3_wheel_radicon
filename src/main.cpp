e4e76553982ff8591fc4a542e84837b48b236f96
Chikurin1014 <chikurin1014@gmail.com>
2024-05-18 23:55:47 +0900

    コメントを変更

--- src/main.cpp
+++ src/main.cpp
@@ -23,14 +23,18 @@ Motordrive motorB{motorB1, motorB2, motorBp, pwmch_B};
 Motordrive motorC{motorC1, motorC2, motorCp, pwmch_C};
 
 void setup() {
-    Serial.begin(115200);           // PCとの通信を開始
-    Ps3.begin("9c:9c:1f:d0:04:be"); // Ps3コントローラとの通信を開始
-    motorA.setup();                 // motorAについて、最初に一回だけ行う処理を実行
-    motorB.setup();                 // motorBについて、最初に一回だけ行う処理を実行
-    motorC.setup();                 // motorCについて、最初に一回だけ行う処理を実行
-    motorA.DRIVE(0);                // motorAを停止 (暴走防止)
-    motorB.DRIVE(0);                // motorBを停止 (暴走防止)
-    motorC.DRIVE(0);                // motorCを停止 (暴走防止)
+    // PCとの通信を開始
+    Serial.begin(115200);
+    // Ps3コントローラとの通信を開始
+    Ps3.begin("9c:9c:1f:d0:04:be");
+    // 各モータについて、最初に一回だけ行う処理を実行
+    motorA.setup();
+    motorB.setup();
+    motorC.setup();
+    // 各モータを停止 (念のため暴走防止)
+    motorA.DRIVE(0);
+    motorB.DRIVE(0);
+    motorC.DRIVE(0);
 }
 
 void loop() {
