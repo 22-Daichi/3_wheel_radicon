7a4d4d85796ffa83cb9376280e01fbe078b185ae
Chikurin1014 <chikurin1014@gmail.com>
2024-05-21 14:35:15 +0900

    rename  variables

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,21 @@
 
 #include "Machine.hpp"
 
-auto inputM = Machine::Model::InputModules{};
-auto outputM = Machine::Model::OutputModules{};
+auto inputModules = Machine::Model::InputModules{};
+auto outputModules = Machine::Model::OutputModules{};
+
 auto state = Machine::DataStructure::State{};
 
 void setup() {
-    inputM.setup();
-    outputM.setup();
+    inputModules.setup();
+    outputModules.setup();
     state = Machine::Model::initialState();
 }
 
 void loop() {
-    const auto input = inputM.read();
-    const auto [s, output] = Machine::Model::update(state, input);
-    state = s;
-    outputM.write(output);
+    const auto input = inputModules.read();
+    const auto [nextState, output] = Machine::Model::update(state, input);
+    state = nextState;
+    outputModules.write(output);
     delay(10);
 }
